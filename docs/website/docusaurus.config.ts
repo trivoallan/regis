8073fbd940cdfee6d9a65a8b8b58e112c71fce02
Tristan Rivoallan <tristan@rivoallan.net>
2026-03-30 10:18:45 +0200

    chore: update Docusaurus base URL and deployment destination to /docs/

--- docs/website/docusaurus.config.ts
+++ docs/website/docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   url: "https://trivoallan.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/regis-cli/",
+  baseUrl: "/regis-cli/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
